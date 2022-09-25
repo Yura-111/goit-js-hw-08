b422ee191f26393c3ccd6cae7372e13e715a0651
Yura <104792276+Yura-111@users.noreply.github.com>
2022-09-25 16:04:44 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,34 +2,7 @@ import throttle from "lodash.throttle";
 
 const formRef = document.querySelector('.feedback-form');
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
-// const formData = [];
 
-
-// function addTextIput (event) {
-//     const message = event.target.value;
-//     const{name, value} = event.target;
-//         formData[name] = value;
-//         localStorage.setItem(LOCAL_STORAGE_KEY, message);
-//         console.log(formData);
-// }
-// formRef.addEventListener('input', addTextIput);
-
-// -------------initPage()
-
-// const addTextIput = event => {
-//     const{name, value} = event.target;
-//     formData[name] = value;
-//     localStorage.setItem(LOCAL_STORAGE_KEY, formData);
-// }
-// formRef.addEventListener('input', addTextIput);
-
-// function initPage() {
-//     const saveData = localStorage.getItem(LOCAL_STORAGE_KEY);
-//     if(saveData) {
-//         const parseData = JSON.parse(saveData)
-//         console.log(saveData);
-//     }
-// ---------}
 
 const save = (key, value) => {
     try {
@@ -64,39 +37,44 @@ const addTextIput = event => {
     const {name, value} = event.target;
     try {
         let saveMassage = load(LOCAL_STORAGE_KEY);
-        saveMassage = saveMassage ? saveMassage : [];
+        saveMassage = saveMassage ? saveMassage : {};
     
         saveMassage[name] = value;
     
         save(LOCAL_STORAGE_KEY, saveMassage);
-        console.log(saveMassage);
     } catch (error) {
         console.log(error);
     }
 }
+
 
 const throttleFormInput = throttle(addTextIput, 1000);
 formRef.addEventListener('input', throttleFormInput);
 
 function textPage() {
     const saveMassage = load(LOCAL_STORAGE_KEY);
-    console.log(saveMassage);
     if(!saveMassage) {
         return;
     }
 
     Object.entries(saveMassage).forEach(([name, value]) => {
         formRef.elements[name].value = value;
-        console.log(name);
-        console.log(value);
     })
     }
 
 const formReset = event => {
     event.preventDefault();
+    const {
+        elements: { email, message },
+    } = event.currentTarget;
+    const formSubmit = {
+        email: email.value,
+        message: message.value,
+    };
+    console.log(formSubmit);
     event.currentTarget.reset();
     remove(LOCAL_STORAGE_KEY);
 }
 
-formRef.addEventListener('resrt', formReset);
+formRef.addEventListener('submit', formReset);
 
